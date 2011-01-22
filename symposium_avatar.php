0d6be8a4252ce2e7b866b911c51738e4cc7f6482
simon.goodchild <simon.goodchild@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-01-22 09:46:55 +0000

    v0.1.29.1
    git-svn-id: https://plugins.svn.wordpress.org/wp-symposium/tags/0.1.29.1@335713 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ symposium_avatar.php
@@ -0,0 +1,109 @@
+<?php
+/*
+Plugin Name: WP Symposium Avatar
+Plugin URI: http://www.wpsymposium.com
+Description: Allows members of a WP Symposium powered site to upload a profile photo. Put [symposium-avatar] on any WordPress page.
+Version: 0.1.29.1
+Author: WP Symposium
+Author URI: http://www.wpsymposium.com
+License: GPL2
+*/
+	
+/*  Copyright 2010,2011  Simon Goodchild  (info@wpsymposium.com)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License, version 2, as 
+    published by the Free Software Foundation.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+function symposium_avatar()  
+{  
+	
+	global $wpdb, $current_user;
+
+	include_once('symposium_styles.php');
+	
+	// Wrapper
+	$html .= "<div id='symposium-wrapper'>";
+
+	$html .= "<h2>".__('Profile Photo')."</h2>";
+	
+	if ($_GET['crop'] != 'y') {
+		
+		$html .= '<p>'.__('Choose an image...', 'wp-symposium').'</p>';
+		
+		$html .= '<input id="file_upload" name="file_upload" type="file" />';
+		
+	} else {
+
+		$db_page = get_site_url().'/wp-content/plugins/wp-symposium/symposium_profile_db.php';	
+	
+		$img = $_GET['img'];
+		$img_path = WP_PLUGIN_DIR.'/wp-symposium/uploads/'.$img;
+
+		if (file_exists($img_path)) {
+			
+			ini_set("memory_limit","100M");
+	
+			include('SimpleImage.php');
+		   	$image = new SimpleImage();
+		   	$image->load($img_path);
+		   	$image->resizeToWidth(600);
+		   	$image->save($img_path);
+		   
+			$html .= '<div style="float:left; margin-bottom:25px">';
+			$html .= '<img src="/wp-content/plugins/wp-symposium/uploads/'.$img.'" id="profile_jcrop_target" /> ';
+			$html .= '</div>';
+			
+			$html .= '<div style="clear: both; float:left;width:100px;height:100px;overflow:hidden;"> ';
+			$html .= '<img src="/wp-content/plugins/wp-symposium/uploads/'.$img.'" id="profile_preview" /> ';
+			$html .= '</div>';
+		
+			$html .= '<div style="margin-left: 20px; float: left;"> ';
+			$html .= '<p>'.__('Select an area above...', 'wp-symposium').'</p>';
+			$html .= '<form action="'.$db_page.'" method="post">';
+				$html .= '<input type="hidden" name="action" value="crop_photo" />';
+				$html .= '<input type="hidden" name="img" value="'.$img.'" />';
+				$html .= '<input type="hidden" name="uid" value="'.$current_user->ID.'" />';
+				$html .= '<input type="hidden" id="x" name="x" />';
+				$html .= '<input type="hidden" id="y" name="y" />';
+				$html .= '<input type="hidden" id="x2" name="x2" />';
+				$html .= '<input type="hidden" id="y2" name="y2" />';
+				$html .= '<input type="hidden" id="w" name="w" />';
+				$html .= '<input type="hidden" id="h" name="h" />';
+				$html .= '<input type="submit" class="button" value="OK" />';
+			$html .= '</form>';
+			$html .= '</div>';
+			
+		} else {
+			
+			$html .= "<p>".$img_path." does not exist, upload probably failed.</p>";
+			
+		}
+		
+	}
+	
+	$html .= '</div>';
+		
+	return $html;
+	
+	exit;
+
+}  
+
+
+
+
+/* ====================================================== SET SHORTCODE ====================================================== */
+add_shortcode('symposium-avatar', 'symposium_avatar');  
+
+?>
---
+++ symposium_register_db.php
@@ -0,0 +1,109 @@
+<?php
+/*  Copyright 2010,2011  Simon Goodchild  (info@wpsymposium.com)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License, version 2, as 
+    published by the Free Software Foundation.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+include_once('../../../wp-config.php');
+include_once('../../../wp-includes/wp-db.php');
+include_once('symposium_functions.php');
+	
+global $wpdb, $current_user;
+wp_get_current_user();
+
+if (!is_user_logged_in()) {
+	
+	$hdn = $_POST['hdn'];
+	$html = "";
+	
+	// Bots tend to fill all fields in, hdn is hidden via CSS
+	if ($hdn == '') {
+		
+		$username = $_POST['username'];
+		$youremail = $_POST['youremail'];
+		$display_name = $_POST['display_name'];
+		$pwd = $_POST['pwd'];
+		$sum1 = $_POST['sum1'];
+		$sum2 = $_POST['sum2'];
+		$result = $_POST['result'];
+		
+		$existing_username = $wpdb->get_var($wpdb->prepare("SELECT user_login FROM ".$wpdb->prefix."users WHERE user_login = '".$username."'"));
+		if ($existing_username) {
+			$html .= 'Sorry, that username is already taken.';
+		} else {
+			$existing_email = $wpdb->get_var($wpdb->prepare("SELECT user_email FROM ".$wpdb->prefix."users WHERE user_email = '".$youremail."'"));
+			if ($existing_email) {
+				$html .= __('That email is already in use by another member, please enter another one.', 'wp-symposium');
+			} else {
+				if ($result != $sum1 + $sum2) {
+					$html .= __('Please enter the sum of the two numbers.', 'wp-symposium');
+				} else {
+					
+					// Store wp_user
+					$wpdb->query( $wpdb->prepare( "
+						INSERT INTO ".$wpdb->prefix."users
+						( 	user_login,
+							user_nicename,
+							user_email, 
+							display_name,
+							user_pass,
+							user_registered
+						)
+						VALUES ( %s, %s, %s, %s, %s, %s )", 
+				        array(
+				        	$username, 
+				        	$username, 
+				        	$youremail,
+				        	$display_name,
+				        	wp_hash_password($pwd),
+				        	date("Y-m-d H:i:s")
+				        	) 
+				        ) );	
+					$new_id = $wpdb->insert_id;
+					
+					// Store wp_usermeta
+				    list($firstname, $lastname) = explode(' ', $display_name); 
+					$wpdb->query( $wpdb->prepare( "INSERT INTO ".$wpdb->prefix."usermeta ( user_id, meta_key, meta_value ) VALUES ( %d, %s, %d )", array($new_id, 'wp_user_level', 0) ) );	
+					$wpdb->query( $wpdb->prepare( "INSERT INTO ".$wpdb->prefix."usermeta ( user_id, meta_key, meta_value ) VALUES ( %d, %s, %s )", array($new_id, 'wp_capabilities', 'a:1:{s:10:"subscriber";s:1:"1";}') ) );	
+					$wpdb->query( $wpdb->prepare( "INSERT INTO ".$wpdb->prefix."usermeta ( user_id, meta_key, meta_value ) VALUES ( %d, %s, %s )", array($new_id, 'first_name', $firstname) ) );	
+					$wpdb->query( $wpdb->prepare( "INSERT INTO ".$wpdb->prefix."usermeta ( user_id, meta_key, meta_value ) VALUES ( %d, %s, %s )", array($new_id, 'last_name', $lastname) ) );	
+					$wpdb->query( $wpdb->prepare( "INSERT INTO ".$wpdb->prefix."usermeta ( user_id, meta_key, meta_value ) VALUES ( %d, %s, %s )", array($new_id, 'nickname', $username) ) );	
+					
+					wp_login($username, $pwd, true);
+			        wp_setcookie($username, $pwd, true);
+			        wp_set_current_user($new_id, $username);
+			        
+			        // Email admin
+			        $body = $display_name." (".$youremail.") ".__('has joined', 'wp-symposium')." ".get_bloginfo('name');
+					symposium_sendmail(get_bloginfo('admin_email'), __('New Member', 'wp-symposium'), $body);
+					
+					// Email new member (if there is a message to send)
+					$message = $wpdb->get_var($wpdb->prepare("SELECT register_message FROM ".$wpdb->prefix.'symposium_config'));
+					if ($message != '') {
+						symposium_sendmail($youremail, __(sprintf('Welcome to %s', get_bloginfo('name')), 'wp-symposium'), str_replace("\n", "<br />", $message));
+					}
+	        
+					header("Location: ".symposium_get_url('profile')."?view=personal");
+					exit;
+				}
+			}
+		}	
+	}
+	$params = "?msg=".$html."&username=".$username."&youremail=".str_replace("@", "!", $youremail)."&display_name=".$display_name;
+	header("Location: ".symposium_get_url('register').$params);
+	exit;
+
+}
+	
+?>
