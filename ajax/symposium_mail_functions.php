843838d3d3a8203126b456072f35f2c2640bae15
simon.goodchild <simon.goodchild@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-02-23 21:01:01 +0000

    0.42
    git-svn-id: https://plugins.svn.wordpress.org/wp-symposium/tags/0.42@350001 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ ajax/symposium_mail_functions.php
@@ -0,0 +1,112 @@
+<?php
+
+include_once('../../../../wp-config.php');
+include_once('../../../../wp-includes/wp-db.php');
+include_once('../symposium_functions.php');
+
+// Get mail messages
+if ($_POST['action'] == 'getBox') {
+	
+	$tray = $_POST["tray"];
+	$term = $_POST["term"];
+
+	if ($tray == "in") {
+		$mail = $wpdb->get_results("SELECT m.*, u.display_name FROM ".$wpdb->base_prefix."symposium_mail m LEFT JOIN ".$wpdb->base_prefix."users u ON m.mail_from = u.ID WHERE mail_in_deleted != 'on' AND mail_to = ".$current_user->ID." AND (u.display_name LIKE '%".$term."%' OR mail_subject LIKE '%".$term."%' OR mail_message LIKE '%".$term."%') ORDER BY mail_mid DESC LIMIT 0,30");
+	}
+
+	$return_arr = array();	
+
+	if ($mail) {
+		foreach ($mail as $item)
+		{
+			if ($item->mail_read != "on") {
+				$row_array['mail_read'] = "row";
+			} else {
+				$row_array['mail_read'] = "row_odd";
+			}
+			$row_array['mail_mid'] = $item->mail_mid;
+			$row_array['mail_sent'] = symposium_time_ago($item->mail_sent);
+			$row_array['mail_from'] = stripslashes(symposium_profile_link($item->mail_from));
+			$row_array['mail_subject'] = stripslashes(symposium_bbcode_remove($item->mail_subject));
+			$row_array['mail_subject'] = preg_replace(
+			  "/(>|^)([^<]+)(?=<|$)/iesx",
+			  "'\\1' . str_replace('" . $term . "', '<span class=\"symposium_search_highlight\">" . $term . "</span>', '\\2')",
+			  $row_array['mail_subject']
+			);
+			$message = stripslashes($item->mail_message);
+			if ( strlen($message) > 75 ) { $message = substr($message, 0, 75)."..."; }
+			$message = preg_replace(
+			  "/(>|^)([^<]+)(?=<|$)/iesx",
+			  "'\\1' . str_replace('" . $term . "', '<span class=\"symposium_search_highlight\">" . $term . "</span>', '\\2')",
+			  $message
+			);
+			$row_array['message'] = symposium_bbcode_remove($message);
+
+	        array_push($return_arr,$row_array);
+
+		}
+	}
+
+	echo json_encode($return_arr);
+
+}
+				
+// Get single mail message
+if ($_POST['action'] == 'getMailMessage') {
+
+	global $wpdb, $current_user;
+	wp_get_current_user();
+	
+	$mail_mid = $_POST['mid'];	
+	$tray = $_POST['tray'];	
+	
+	$message = get_message($mail_mid, $tray);
+	
+	// Fetch new unread count
+	$unread = $wpdb->get_var("SELECT COUNT(*) FROM ".$wpdb->prefix.'symposium_mail'." WHERE mail_to = ".$current_user->ID." AND mail_".$tray."_deleted != 'on' AND mail_read != 'on'");
+
+	echo $mail_mid."[split]".$unread."[split]".$tray."[split]".$message;
+	exit;
+}
+
+// Compose - recipients list
+if ($_GET['term'] != '') {
+	
+	global $wpdb;	
+	$return_arr = array();
+
+	$list = $wpdb->get_results("SELECT u.ID, u.display_name, m.city, m.country FROM ".$wpdb->base_prefix."users u LEFT JOIN ".$wpdb->base_prefix."symposium_usermeta m ON u.ID = m.uid WHERE (u.display_name LIKE '".$_GET['term']."%') OR (m.city LIKE '".$_GET['term']."%') OR (m.country LIKE '".$_GET['term']."%') OR (u.display_name LIKE '% %".$_GET['term']."%') ORDER BY u.display_name");
+
+	if ($list) {
+		foreach ($list as $item) {
+			$row_array['id'] = $item->ID;
+			$row_array['value'] = $item->display_name;
+			$row_array['label'] = $item->display_name;
+			if ($item->city != '') {
+				$row_array['city'] = $item->city;
+			} else {
+				$row_array['city'] = '';
+			}
+			if ($item->country != '') {
+				if ($item->city != '') {
+					$row_array['country'] = ', '.$item->country;
+				} else {
+					$row_array['country'] = $item->country;
+				}
+			} else {
+				$row_array['country'] = '';
+			}
+			
+	        array_push($return_arr,$row_array);
+		}
+	}
+
+	echo json_encode($return_arr);
+	exit;
+
+}
+
+
+?>
+
+	
---
+++ uploadify/uploadify.php
@@ -0,0 +1,73 @@
+<?php
+/*
+Uploadify v2.1.4
+Release Date: November 8, 2010
+
+Copyright (c) 2010 Ronnie Garcia, Travis Nickels
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+*/
+
+if (!empty($_FILES)) {
+
+	include_once('../../../../wp-config.php');
+
+	$tempFile = $_FILES['Filedata']['tmp_name'];
+
+	if ( WPS_TMP_DIR != "WPS_TMP_DIR" ) {
+		$targetPath = WPS_TMP_DIR.'/';
+	} else {
+		$targetPath = WP_PLUGIN_DIR.'/wp-symposium/uploads/';
+	}
+
+	$filename = $_FILES['Filedata']['name'];
+	$filename = preg_replace('/[^A-Za-z0-9.]/','_',$filename);
+	$targetFile =  str_replace('//','/',$targetPath) . $filename;
+	
+	if (!file_exists($targetPath)) {
+		mkdir(str_replace('//','/',$targetPath), 0755, true);
+	}
+
+	if (file_exists($targetPath)) {
+
+		if (file_exists($tempFile)) {
+		
+			if (move_uploaded_file($tempFile,$targetFile)) {
+				echo $filename;
+				exit;
+			} else {
+				echo "FAILED: Could not move ".$tempFile." ".$targetFile;
+				exit;
+			};
+
+		} else {
+			echo "FAILED: Could not find temporary file ".$tempFile;
+			exit;
+		}
+		
+	} else {
+		echo "FAILED: Could not create ".$targetPath." (".WPS_TMP_DIR.")";
+		exit;
+	}
+
+} else {
+	echo "FAILED: file did not upload at all";
+	exit;
+}
+?>
