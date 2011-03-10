2e59702f60a8f4781d065326456801e2eff518e0
simon.goodchild <simon.goodchild@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-03-10 13:55:32 +0000

    0.48.1
    git-svn-id: https://plugins.svn.wordpress.org/wp-symposium/tags/0.48.1@358244 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ symposium_forum.php
@@ -0,0 +1,55 @@
+<?php
+/*
+Plugin Name: WP Symposium Forum
+Plugin URI: http://www.wpsymposium.com
+Description: Forum component for the Symposium suite of plug-ins. Put [symposium-forum] on any WordPress page to display forum.
+Version: 0.48.1
+Author: WP Symposium
+Author URI: http://www.wpsymposium.com
+License: GPL3
+*/
+	
+/*  Copyright 2010,2011  Simon Goodchild  (info@wpsymposium.com)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License, version 2, as 
+    published by the Free Software Foundation.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+function symposium_forum() {	
+
+	$html = '';
+		
+	// Wrapper
+	$html .= "<div class='symposium-wrapper'>";
+
+		$html .= "<div id='symposium-forum-div'></div>";
+
+	$html .= "</div>";
+	// End Wrapper
+	
+	$html .= "<div style='clear: both'></div>";
+	
+	// Send HTML
+	return $html;
+
+}
+
+/* ====================================================== SET SHORTCODE ====================================================== */
+
+if (!is_admin()) {
+	add_shortcode('symposium-forum', 'symposium_forum');  
+}
+
+
+
+?>
