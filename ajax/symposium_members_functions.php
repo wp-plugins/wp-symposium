34e915f21f5a86ff4dcd0df89ff0a5cda08e7fab
simon.goodchild <simon.goodchild@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-03-21 17:25:48 +0000

    0.49.7
    git-svn-id: https://plugins.svn.wordpress.org/wp-symposium/tags/0.49.7@362953 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ ajax/symposium_members_functions.php
@@ -0,0 +1,135 @@
+<?php
+
+include_once('../../../../wp-config.php');
+
+global $wpdb, $current_user;
+wp_get_current_user();
+
+// Members list search
+if (isset($_GET['term']) && $_GET['term'] != '') {
+	
+	global $wpdb;	
+	$return_arr = array();
+
+	$list = $wpdb->get_results("SELECT u.ID, u.display_name, m.city, m.country FROM ".$wpdb->base_prefix."users u LEFT JOIN ".$wpdb->base_prefix."symposium_usermeta m ON u.ID = m.uid WHERE (u.display_name LIKE '".$_GET['term']."%') OR (m.city LIKE '".$_GET['term']."%') OR (m.country LIKE '".$_GET['term']."%') OR (u.display_name LIKE '% %".$_GET['term']."%') ORDER BY u.display_name");
+	
+	if ($list) {
+		foreach ($list as $item) {
+			$row_array['id'] = $item->ID;
+			$row_array['value'] = $item->ID;
+			$row_array['label'] = $item->display_name;
+			if ($item->city != '') {
+				$row_array['city'] = $item->city;
+			} else {
+				$row_array['city'] = '';
+			}
+			if ($item->country != '') {
+				if ($item->city != '') {
+					$row_array['country'] = ', '.$item->country;
+				} else {
+					$row_array['country'] = $item->country;
+				}
+			} else {
+				$row_array['country'] = '';
+			}
+			$row_array['avatar'] = get_avatar($item->ID, 40);
+			
+	        array_push($return_arr,$row_array);
+		}
+	}
+
+	echo json_encode($return_arr);
+	exit;
+
+}
+
+// AJAX function to get members
+if ($_POST['action'] == 'getMembers') {
+
+	$html = '';
+	$page = $_POST['page'];
+	$page_length = 25;
+	$me = $_POST['me'];
+
+	$plugin_dir = WP_PLUGIN_URL.'/wp-symposium/';
+	$config = $wpdb->get_row($wpdb->prepare("SELECT online,offline FROM ".$wpdb->prefix . 'symposium_config'));
+	
+	$sql = "SELECT m.uid, m.last_activity, m.status, m.city, m.country, m.share, m.wall_share,
+	(SELECT comment FROM ".$wpdb->base_prefix."symposium_comments WHERE author_uid = m.uid AND subject_uid = author_uid and comment_parent = 0 ORDER BY cid DESC LIMIT 0,1) AS latest_comment, 
+	(SELECT COUNT(*) FROM ".$wpdb->base_prefix."symposium_friends WHERE friend_from = ".$me." AND friend_to = m.uid) AS is_friend 
+	FROM ".$wpdb->base_prefix."symposium_usermeta m 
+	WHERE m.uid > 0 
+	ORDER BY m.last_activity DESC LIMIT ".($page*$page_length-$page_length).",".$page_length;
+	
+	$members = $wpdb->get_results($sql);
+
+	if ($members) {
+		
+		$inactive = $config->online;
+		$offline = $config->offline;
+		$profile = symposium_get_url('profile');
+		
+		foreach ($members as $member) {
+			
+			$time_now = time();
+			if ($member->last_activity && $member->status != 'offline') {
+				$last_active_minutes = convert_datetime($member->last_activity);
+				$last_active_minutes = floor(($time_now-$last_active_minutes)/60);
+			} else {
+				$last_active_minutes = 999999999;
+			}
+														
+			$html .= "<div class='members_row";
+			if ($member->is_friend == 1 || $member->uid == $me) {
+				$html .= " row corners'>";		
+			} else {
+				$html .= " row_odd corners'>";		
+			}
+				$html .= "<div class='members_info'>";
+
+					if ( ($member->uid == $me) || (strtolower($member->share) == 'everyone') || (strtolower($member->share) == 'friends only' && $member->is_friend) ) {
+						$html .= "<div class='members_location'>";
+							if ($member->city != '') {
+								$html .= $member->city;
+							}
+							if ($member->country != '') {
+								if ($member->city != '') {
+									$html .= ', '.$member->country;
+								} else {
+									$html .= $member->country;
+								}
+							}
+						$html .= "</div>";
+					}
+
+					$html .= "<div class='members_avatar'>";
+						$html .= get_avatar($member->uid, 64);
+					$html .= "</div>";
+					$html .= symposium_profile_link($member->uid).', last active '.symposium_time_ago($member->last_activity).". ";
+					if ($last_active_minutes >= $offline) {
+						//$html .= '<img src="'.$plugin_dir.'images/loggedout.gif">';
+					} else {
+						if ($last_active_minutes >= $inactive) {
+							$html .= '<img src="'.$plugin_dir.'images/inactive.gif">';
+						} else {
+							$html .= '<img src="'.$plugin_dir.'images/online.gif">';
+						}
+					}
+					if ( ($member->uid == $me) || (strtolower($member->wall_share) == 'everyone') || (strtolower($member->wall_share) == 'friends only' && $member->is_friend) ) {
+						$html .= "<br />".stripslashes($member->latest_comment);
+					}
+				$html .= "</div>";
+			$html .= "</div>";
+		}
+	} else {
+		$html = "No members....";
+	}
+	
+	echo $html;
+	exit;
+}
+
+
+?>
+
+	
---
+++ symposium_mail.php
@@ -0,0 +1,106 @@
+<?php
+/*
+Plugin Name: WP Symposium Mail
+Plugin URI: http://www.wpsymposium.com
+Description: Mail component for the Symposium suite of plug-ins. Put [symposium-mail] on any WordPress page.
+Version: 0.49.7
+Author: WP Symposium
+Author URI: http://www.wpsymposium.com
+License: GPL3
+*/
+	
+/*  Copyright 2010,2011  Simon Goodchild  (info@wpsymposium.com)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License, version 2, as 
+    published by the Free Software Foundation.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+function symposium_mail() {	
+	
+	global $wpdb, $current_user;
+	wp_get_current_user();
+
+	$thispage = get_permalink();
+	if ($thispage[strlen($thispage)-1] != '/') { $thispage .= '/'; }
+	$mail_url = $wpdb->get_var($wpdb->prepare("SELECT mail_url FROM ".$wpdb->prefix . 'symposium_config'));
+
+	if (isset($_GET['page_id']) && $_GET['page_id'] != '') {
+		// No Permalink
+		$thispage = $mail_url;
+		$q = "&";
+	} else {
+		$q = "?";
+	}
+	
+	$plugin_dir = WP_PLUGIN_URL.'/wp-symposium/';
+	
+	if (is_user_logged_in()) {
+
+		$template = $wpdb->get_var("SELECT template_mail FROM ".$wpdb->prefix."symposium_config");
+		$template = str_replace("[]", "", stripslashes($template));
+
+		$html = '<div class="symposium-wrapper">'.$template.'</div>';
+			
+		// Compose Form	
+		$compose = '<div id="compose_form" style="display:none">';
+	
+			$compose .= '<div class="floatright send_button">';
+			$compose .= '<input id="mail_send_button" type="submit" class="symposium-button" value="'.__('Send', 'wp-symposium').'" />';
+			$compose .= '<input id="mail_cancel_button" type="submit" class="symposium-button" value="'.__('Cancel', 'wp-symposium').'" />';
+			$compose .= '</div>';
+
+			$compose .= '<div id="compose_mail_to">';
+				$compose .= '<div class="new-topic-subject label">'.__('Start typing the recipient\'s name...', 'wp-symposium').'</div>';
+ 				$compose .= "<input type='text' id='compose_recipient' class='new-topic-subject-input' style='width:50%' value=''/>";
+ 			$compose .= '</div>';
+			
+			$compose .= '<div id="compose_mail_subject">';
+				$compose .= '<div class="new-topic-subject label">'.__('Subject', 'wp-symposium').'</div>';
+ 				$compose .= "<input type='text' id='compose_subject' class='new-topic-subject-input' value='' />";
+ 			$compose .= '</div>';
+			
+			$compose .= '<div id="compose_mail_message">';
+				$compose .= '<div class="new-topic-subject label">'.__('Message', 'wp-symposium').'</div>';
+				$compose .= '<textarea class="reply-topic-subject-text" id="compose_text"></textarea>';
+ 			$compose .= '</div>';
+			
+			$compose .= '<input type="hidden" id="compose_previous" value="" />';
+	
+		$compose .= "</div>";
+		
+		// Replace template codes
+		$html = str_replace("[compose_form]", $compose, stripslashes($html));
+		$html = str_replace("[compose]", __("Compose", "wp-symposium"), stripslashes($html));
+		$html = str_replace("[inbox]", __("In Box", "wp-symposium"), stripslashes($html));
+		$html = str_replace("[sent]", __("Sent", "wp-symposium"), stripslashes($html));
+		
+
+	} else {
+		// Not logged in
+		$html .= __('You have to login to access your mail.', 'wp-symposium');
+	}
+	
+	// Send HTML
+	return $html;
+
+}
+
+/* ====================================================== SET SHORTCODE ====================================================== */
+
+if (!is_admin()) {
+	add_shortcode('symposium-mail', 'symposium_mail');  
+}
+
+
+
+?>
